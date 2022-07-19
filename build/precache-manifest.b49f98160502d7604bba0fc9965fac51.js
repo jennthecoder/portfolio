2a63de7ed751ecbcbfa4a1205158c84bfdbf3250
Jennifer Nnadi <jennify.nnadi@gmail.com>
2022-07-19 17:28:40 -0400

    updated build

---
+++ build/precache-manifest.b49f98160502d7604bba0fc9965fac51.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e56c29f26a072b0f94be6f281dd62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2a4b5b0086a68072e1",
+    "url": "/static/css/main.ea8f3155.chunk.css"
+  },
+  {
+    "revision": "efc888ff7156df8711eb",
+    "url": "/static/js/2.714c1746.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.714c1746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2a4b5b0086a68072e1",
+    "url": "/static/js/main.7434160d.chunk.js"
+  },
+  {
+    "revision": "7707b614a8fbf8e576c2",
+    "url": "/static/js/runtime-main.131e43d9.js"
+  },
+  {
+    "revision": "4395b29d9210bd75051ebce1e66e186d",
+    "url": "/static/media/AirbnbCereal-Black.4395b29d.ttf"
+  },
+  {
+    "revision": "c94c29feb80b02db176028d16fc634d2",
+    "url": "/static/media/AirbnbCereal-Bold.c94c29fe.ttf"
+  },
+  {
+    "revision": "86b15ac031e0507ad790afafd9adf7f7",
+    "url": "/static/media/AirbnbCereal-Book.86b15ac0.ttf"
+  },
+  {
+    "revision": "0a0cbe1eca15f71b7187b38b1173eb98",
+    "url": "/static/media/AirbnbCereal-ExtraBold.0a0cbe1e.ttf"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "/static/media/AirbnbCereal-Light.1f9c7d32.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/static/media/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "d8dc808e57a3acfe2544436db034e269",
+    "url": "/static/media/menu-line.d8dc808e.svg"
+  },
+  {
+    "revision": "d1728a55c61903974365f3f343058e91",
+    "url": "/static/media/open.d1728a55.svg"
+  }
+]);
